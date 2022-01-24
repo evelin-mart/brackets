375e9c2d9d2474ed4fb55f8f1fdff4e9f7f437d4
evelin-mart <evalouis666@gmail.com>
2022-01-25 00:20:45 +0300

    feat: add answer'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  let pairs = {};
+  bracketsConfig.forEach((pair) => {
+    pairs[pair[0]] = pair[1];
+  });
+
+  for (i = 0; i < str.length; i++) {
+    let bracket = str[i];
+    let openBracket = stack[stack.length - 1];
+
+    if (pairs[bracket] === bracket) {
+      if (openBracket === bracket) {
+        stack.pop();
+        continue;
+      } else {
+        stack.push(bracket);
+        continue;
+      }
+    }
+
+    if (Object.keys(pairs).includes(bracket)) {
+      stack.push(bracket);
+    }
+
+    if (Object.values(pairs).includes(bracket)) {
+      if (pairs[openBracket] === bracket) {
+        stack.pop();
+      } else return false;
+    }
+  }
+
+  return !stack.length;
+};
